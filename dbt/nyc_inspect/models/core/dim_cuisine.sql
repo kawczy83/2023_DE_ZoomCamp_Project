3f0c11802ce2d88b7557a926b5a565cc381b6903
kawczy83 <kawczy83@gmail.com>
2023-04-09 11:02:33 -0400

    Update and rename dim_name.sql to dim_cuisine.sql

--- dbt/nyc_inspect/models/core/dim_cuisine.sql
+++ dbt/nyc_inspect/models/core/dim_cuisine.sql
@@ -5,17 +5,12 @@
 {{ config(materialized='table') }}
 
 select
-    row_number() over(order by cuisine_description) as name_id,
-    camis,
-    dba,
-    cuisine_description,
-    phone
+    row_number() over(order by cuisine_description) as cuisine_id,
+    cuisine_description
 from
 (    select
-      distinct camis,
-      dba,
+      distinct
       cuisine_description,
-      phone
     from {{ ref('stg_nyc_inspect') }}
      )
 
